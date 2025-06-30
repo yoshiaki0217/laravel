ad5b39235c8da58425ee325782e81b43e73df5a4
大城由晃 <yoshiaki19980217@icloud.com>
2025-07-01 00:08:52 +0900

    修正

--- src/vite.config.js
+++ src/vite.config.js
@@ -10,4 +10,13 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true
+        }
+    }
 });
